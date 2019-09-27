0eb9571857c947e536b640ec990f746c98e9ac77
Cheol-Ho Cho <hoonchcho@gmail.com>
2019-09-27 10:03:49 +0900

    least squares: data transformation

--- LinearRegression(LeastSquares) practice.py
+++ LinearRegression(LeastSquares) practice.py
@@ -14,10 +14,13 @@ y = data[:int(data.shape[0]/2),1] #use only male's label
 X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size = 0.2, random_state = 42)
 
-X_train = X_train.reshape(-1,1)
+print(X_train)
+
+X_train = X_train.reshape(-1,1) # transformation for fitting linear models
 X_test = X_test.reshape(-1,1)
 y_train = y_train.reshape(-1,1)
 y_test = y_test.reshape(-1,1)
+print(X_train)
 
 # Create linear regression object
 regr = linear_model.LinearRegression()
